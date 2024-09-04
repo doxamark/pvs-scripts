57ac68350688d0508ecd7b83b4cd66049b4763d6
PVSMark <pvsscripts@propertyvaluationservices.net>
2024-09-04 15:58:37 -0500

    feat: add additional tbs script for prod

--- src/scripts/tax_bill_scripts/counties/OrangeCountyPP.js
+++ src/scripts/tax_bill_scripts/counties/OrangeCountyPP.js
@@ -6,7 +6,7 @@ class OrangeCountyPPScript extends BaseScript {
     async performScraping() {
         await this.page.goto(this.accountLookupString, { waitUntil: 'networkidle2' });
         console.log(`Navigated to: ${this.page.url()}`);
-
+        await new Promise(resolve => setTimeout(resolve, 2000));
         const targetYear = this.year;
 
         // Select the desired year in the dropdown
@@ -22,19 +22,25 @@ class OrangeCountyPPScript extends BaseScript {
         }
 
         if (!isBillYearAvailable) {
-            this.outputPath = null
+            console.error('No Bills Found With Target Year. Please check your account number.', this.account);
+            await new Promise(resolve => setTimeout(resolve, 3000));
             return { is_success: false, msg: `Target year is not available` };
         }
 
+        const billNotEmpty = await this.page.$('.buttonccp');
+
+        if (!billNotEmpty) {
+            console.error('No Bills Found With Target Year. Please check your account number.', this.account);
+            await new Promise(resolve => setTimeout(resolve, 3000));
+            return { is_success: false, msg: `No Bills Found With Target Year. Please check your account number. ${this.account}` };
+        }
+
+        await this.page.waitForSelector('input[type="button"][value="Go"]');
+    
         return { is_success: true, msg: `` };
     }
 
     async saveAsPDF() {
-
-        if (!this.outputPath) {
-            return;
-        }
-
         const dir = path.dirname(this.outputPath);
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir, { recursive: true });
@@ -50,10 +56,8 @@ class OrangeCountyPPScript extends BaseScript {
         await new Promise(resolve => setTimeout(resolve, 2000));
         await this.page.click(billLinkSelector, { clickCount: 1 });
 
-        await new Promise(resolve => setTimeout(resolve, 3000));
-
         // Wait for the file to download
-        while (!fs.existsSync(customPath) || fs.readdirSync(customPath).length === 0) {
+        while (!fs.existsSync(customPath) || fs.readdirSync(customPath).length === 0 && this.hasPdfFiles(customPath)) {
             await new Promise(resolve => setTimeout(resolve, 1000));
         }
 
@@ -62,12 +66,35 @@ class OrangeCountyPPScript extends BaseScript {
         if (files.length > 0) {
             const downloadedFile = path.join(customPath, files[0]);
             const outputFilePath = path.resolve(this.outputPath);
-            fs.renameSync(downloadedFile, outputFilePath);
-            fs.rmdirSync(customPath);
+            try {
+                // Attempt to rename the file
+                fs.renameSync(downloadedFile, outputFilePath);
+              } catch (err) {
+                if (err.code === 'EXDEV') {
+                  // Handle cross-device link error by copying and then deleting
+                  fs.copyFileSync(downloadedFile, outputFilePath);
+                  fs.unlinkSync(downloadedFile);
+                } else {
+                  throw err; // Re-throw error if it's not an EXDEV error
+                }
+              }
+        
+              // Remove the directory if needed
+              fs.rmSync(customPath, { recursive: true });
         }
 
         console.log(`PDF saved: ${this.outputPath}`);
 
+    }
+
+    hasPdfFiles(dir) {
+        const files = fs.readdirSync(dir);
+        return files.some(file => {
+            const extname = path.extname(file).toLowerCase();
+            const basename = path.basename(file, extname);
+            // Check if the file has a .pdf extension and does not have additional extensions
+            return extname === '.pdf' && !basename.includes('.crdownload');
+        });
     }
 }
 
