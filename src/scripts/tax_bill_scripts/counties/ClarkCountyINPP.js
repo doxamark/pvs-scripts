ab521dad13a4272518fad305dafcbad545cd9868
PVSMark <pvsscripts@propertyvaluationservices.net>
2024-10-23 05:52:14 -0500

    feat: add scripts for one county

---
+++ src/scripts/tax_bill_scripts/counties/ClarkCountyINPP.js
@@ -0,0 +1,118 @@
+import BaseScript from '../../../core/BaseScript.js';
+import fs from 'fs';
+import path from 'path';
+
+class ClarkCountyINPPScript extends BaseScript {
+    async performScraping() {
+        await this.page.goto(this.accountLookupString, { waitUntil: 'networkidle2' });
+        console.log(`Navigated to: ${this.page.url()}`);
+        
+        await Promise.race([
+            this.page.waitForSelector('.default.search-results-public', { visible: true }),
+            this.page.waitForSelector('.message', { visible: true })
+        ]);
+
+        const errors = await this.page.$$(".message")
+        let noBill = false;
+        for (let err of errors) {
+            const label = await this.page.evaluate(el => el.textContent, err);
+            if (label.trim().includes("No Results")) {
+                noBill = true
+                break
+            }
+        }
+
+        if (noBill) {
+            console.error('No Bills Found. Please check your account number.', this.account);
+            return { is_success: false, msg: `No Bills Found. Please check your account number. ${this.account}` };
+        }
+
+        const results = await this.page.$$('.default.search-results-public a');
+        let billLink = null
+        for (let result of results) {
+            const text = await this.page.evaluate(el => el.textContent, result);
+            if (text.trim().includes(`Download`)) {
+                billLink = result
+                break
+            }
+        }
+
+        if (!billLink) {
+            console.error('No Bills Found. Please check your account number.', this.account);
+            return { is_success: false, msg: `No Bills Found. Please check your account number. ${this.account}` };
+        }
+
+        return { is_success: true, msg: `` };
+    }
+
+    async saveAsPDF() {
+        const dir = path.dirname(this.outputPath);
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        const customPath = path.resolve(`src/temp/${this.account}`);
+        const client = await this.page.createCDPSession();
+        await client.send('Page.setDownloadBehavior', {
+            behavior: 'allow', downloadPath: customPath
+        });
+
+        const results = await this.page.$$('.default.search-results-public a');
+        let billLink = null
+        for (let result of results) {
+            const text = await this.page.evaluate(el => el.textContent, result);
+            if (text.trim().includes(`Download`)) {
+                billLink = result
+                break
+            }
+        }
+
+        if (!billLink) {
+            console.error('No Bills Found. Please check your account number.', this.account);
+            return { is_success: false, msg: `No Bills Found. Please check your account number. ${this.account}` };
+        }
+
+        await billLink.click()
+
+        // Wait for the file to download
+        while (!fs.existsSync(customPath) || fs.readdirSync(customPath).length === 0 || !this.hasPdfFiles(customPath)) {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+
+        // Rename the file inside the customPath to this.outputPath
+        const files = fs.readdirSync(customPath);
+        if (files.length > 0) {
+            const downloadedFile = path.join(customPath, files[0]);
+            const outputFilePath = path.resolve(this.outputPath);
+            try {
+                // Attempt to rename the file
+                fs.renameSync(downloadedFile, outputFilePath);
+              } catch (err) {
+                if (err.code === 'EXDEV') {
+                  // Handle cross-device link error by copying and then deleting
+                  fs.copyFileSync(downloadedFile, outputFilePath);
+                  fs.unlinkSync(downloadedFile);
+                } else {
+                  throw err; // Re-throw error if it's not an EXDEV error
+                }
+              }
+        
+              // Remove the directory if needed
+              fs.rmSync(customPath, { recursive: true });
+        }
+
+        console.log(`PDF saved: ${this.outputPath}`);
+
+    }
+
+    hasPdfFiles(dir) {
+        const files = fs.readdirSync(dir);
+        return files.some(file => {
+            const extname = path.extname(file).toLowerCase();
+            const basename = path.basename(file, extname);
+            // Check if the file has a .pdf extension and does not have additional extensions
+            return extname === '.pdf' && !extname.includes('.crdownload');
+        });
+    }
+}
+
+export default ClarkCountyINPPScript;
---
+++ src/scripts/tax_bill_scripts/counties/ClarkCountyINRE.js
@@ -0,0 +1,8 @@
+
+import ClarkCountyINPPScript from './ClarkCountyINPP.js';
+
+class ClarkCountyINREScript extends ClarkCountyINPPScript {
+
+}
+
+export default ClarkCountyINREScript;
